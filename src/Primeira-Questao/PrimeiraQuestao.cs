57830930171ca813059c2c7557a7bdb316719177
Guian Rocha <guian.rocha@t-systems.com>
2024-09-19 08:00:21 -0300

    Primeira resposta OK

---
+++ src/Primeira-Questao/PrimeiraQuestao.cs
@@ -0,0 +1,22 @@
+﻿namespace Primeira_Questao
+{
+    public static class PrimeiraQuestao
+    {
+        public static int PrimeiraPergunta()
+        {
+            int indice = 12;
+            int soma = 0;
+            int k = 1;
+            int result = 0;
+
+            while (k < indice)
+            {
+                k = k + 1;
+                soma = soma + k;
+                Console.WriteLine($"{soma}");
+                result = soma;
+            }
+            return result;
+        }
+    }
+}
---
+++ src/RespostasFinais/Program.cs
@@ -0,0 +1,12 @@
+﻿using Primeira_Questao;
+using Segunda_Questao;
+
+int respostaPrimeiraQuestao = PrimeiraQuestao.PrimeiraPergunta();
+
+Console.WriteLine($"Resposta da primeira questão: {respostaPrimeiraQuestao}");
+Console.WriteLine("========================================================");
+
+Console.WriteLine("Mostrando sequencias: ");
+
+
+Console.WriteLine($"a) {SegundaQuestao.SubQuestaoA()}");

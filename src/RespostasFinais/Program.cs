03a67c726a3b1e2b62262a36082369ae8a3875e0
Guian Rocha <guian.rocha@t-systems.com>
2024-09-19 08:34:20 -0300

    Segunda resposta OK

--- src/RespostasFinais/Program.cs
+++ src/RespostasFinais/Program.cs
@@ -7,6 +7,24 @@ Console.WriteLine($"Resposta da primeira questão: {respostaPrimeiraQuestao}");
 Console.WriteLine("========================================================");
 
 Console.WriteLine("Mostrando sequencias: ");
+SegundaQuestao.SubQuestaoA();
+Console.WriteLine();
+SegundaQuestao.SubQuestaoB();
+Console.WriteLine();
+SegundaQuestao.SubQuestaoC();
+Console.WriteLine();
+SegundaQuestao.SubQuestaoD();
+Console.WriteLine();
+SegundaQuestao.SubQuestaoE();
+Console.WriteLine();
+SegundaQuestao.SubQuestaoF();
+Console.WriteLine();
 
 
-Console.WriteLine($"a) {SegundaQuestao.SubQuestaoA()}");
+Console.WriteLine("Respostas finais:");
+Console.WriteLine($"A) 9");
+Console.WriteLine($"B) 128");
+Console.WriteLine($"C) 49");
+Console.WriteLine($"D) 100");
+Console.WriteLine($"E) 13");
+Console.WriteLine($"F) 20");
---
+++ src/Segunda-Questao/SegundaQuestao.cs
@@ -0,0 +1,96 @@
+﻿namespace Segunda_Questao
+{
+    public class SegundaQuestao
+    {
+        public static void SubQuestaoA()
+        {
+            int[] valor = new int[5];
+
+            valor[0] = 1;
+
+            for (int i = 1; i < valor.Length; i++)
+            {
+                valor[i] = valor[i - 1] + 2;
+            }
+
+            foreach (var valorAtual in valor)
+            {
+                Console.Write($"{valorAtual} ");
+            }            
+        }
+
+        public static void SubQuestaoB()
+        {
+            int[] valor = new int[7];
+            valor[0] = 2;
+
+            for (int i = 1; i < valor.Length; i++)
+            {
+                valor[i] = valor[i - 1] * 2;
+            }
+
+            foreach (var valorAtual in valor)
+            {
+                Console.Write($"{valorAtual} ");
+            }
+        }
+
+        public static void SubQuestaoC()
+        {
+            int[] valor = new int[8];
+
+            for (int i = 0; i < valor.Length; i++)
+            {
+                valor[i] = i * i;
+            }
+
+            foreach (var valorAtual in valor)
+            {
+                Console.Write($"{valorAtual} ");
+            }
+        }
+
+        public static void SubQuestaoD()
+        {
+            int[] valor = new int[5];
+            int par = 2;
+
+            for (int i = 0; i < valor.Length; i++)
+            {
+                valor[i] = par * par;
+                par += 2;
+            }
+
+            foreach (var valorAtual in valor)
+            {
+                Console.Write($"{valorAtual} ");
+            }
+        }
+
+        public static void SubQuestaoE()
+        {
+            int[] valor = new int[7];
+            valor[0] = 1;
+            valor[1] = 1;
+
+            for (int i = 2; i < valor.Length; i++)
+            {
+                valor[i] = valor[i - 1] + valor[i - 2];
+            }
+
+            foreach (var valorAtual in valor)
+            {
+                Console.Write($"{valorAtual} ");
+            }
+        }
+
+        public static void SubQuestaoF()
+        {
+            int[] valor = [2, 10, 12, 16, 17, 18, 19, 20];
+            foreach (var valorAtual in valor)
+            {
+                Console.Write($"{valorAtual} ");
+            }
+        }
+    }
+}
